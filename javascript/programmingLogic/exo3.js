520c0266b784606369b19bf149ec72431b4e18e0
danyelleac <danyelleacandido@gmail.com>
2023-03-09 09:05:45 -0300

    exos-programming-logic

---
+++ javascript/programmingLogic/exo3.js
@@ -0,0 +1,9 @@
+// escreva uma função que recebe o numero n
+//retorno o seguinte:
+//Nº divisivel por 3=
+//N« divisivel por 5 =
+//Nº divisivel por 3 e 5 =
+//Nº não é divisivel por 3 e 5 = retorna o próprio número
+//Checar se o numero é realmente um numero
+//use a função com nº de 0 a 100
+
