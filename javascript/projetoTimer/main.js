9b3b403f962d81b78cacc3e9c56f4334b34ec22c
danyelleac <danyelleacandido@gmail.com>
2023-03-28 14:01:55 -0300

    style

--- javascript/projetoTimer/main.js
+++ javascript/projetoTimer/main.js
@@ -1,1 +1,31 @@
-const 
+let sec =0;
+let timerInit;
+const timer = document.querySelector('.timer')
+const startTimer = document.querySelector('.startTimer')
+const pauseTimer = document.querySelector('.pauseTimer')
+const cleanTimer = document.querySelector('.cleanTimer')
+
+function formatTimer(sec){
+    const date = new Date(sec *1000);
+    return date.toLocaleTimeString('pt-BR',{
+        hour12:false,
+        timeZone:'UTC'
+    })
+}
+
+export function startTimer (){
+    timerInit = setInterval(function (){
+        sec++;
+        timer.innerHTML = formatTimer(sec);
+    },100);
+}
+
+export const pauseTimer = ()=>{
+    return console.log("hey")
+}
+
+export const cleanTimer = ()=>{
+    return console.log("hi")
+}
+
+
