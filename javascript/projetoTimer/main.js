06964d0719058e9fd410b387a597426a7aaeabde
danyelleac <danyelleacandido@gmail.com>
2023-04-04 14:29:02 -0300

    config-timer

--- javascript/projetoTimer/main.js
+++ javascript/projetoTimer/main.js
@@ -1,31 +1,50 @@
-let sec =0;
-let timerInit;
 const timer = document.querySelector('.timer')
 const startTimer = document.querySelector('.startTimer')
 const pauseTimer = document.querySelector('.pauseTimer')
 const cleanTimer = document.querySelector('.cleanTimer')
+let sec = 0
+let updateTimer
 
+function initTimer (){
+    updateTimer = setInterval(function(){
+        sec ++;
+        timer.innerHTML = formatTimer(sec)
+    },1000);
+
+}
 function formatTimer(sec){
     const date = new Date(sec *1000);
     return date.toLocaleTimeString('pt-BR',{
         hour12:false,
-        timeZone:'UTC'
+        timeZone:'UTC'//a data é 01/01/70 com a hora 00:00:00
     })
 }
 
-export function startTimer (){
-    timerInit = setInterval(function (){
-        sec++;
-        timer.innerHTML = formatTimer(sec);
-    },100);
-}
+startTimer.addEventListener('click',function(event){
+    timer.classList.remove('pause')
+    clearInterval(updateTimer)
+    initTimer()
+});
 
-export const pauseTimer = ()=>{
-    return console.log("hey")
-}
+pauseTimer.addEventListener('click',function(event){
+    clearInterval(updateTimer)
+    timer.classList.add('pause')
+});
 
-export const cleanTimer = ()=>{
-    return console.log("hi")
-}
+cleanTimer.addEventListener('click',function(event){
+    timer.classList.remove('pause')
+    clearInterval(updateTimer)
+    sec = 0
+    timer.innerHTML = '00:00:00'
 
+});
 
+/* ou posso usar o document.addEventListener('click', function(e){
+    const el = e.target;
+
+    if(el.classList.contains('zerar')){
+        clearInterval(timer)
+        timer.innerHTML = '00:00:00',
+        sec = 0
+    }
+}) */
